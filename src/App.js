fe55d6de99fa1b7544b80fee70d4237f7e438e79
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-12-05 15:33:06 +0300

    memo clearbutton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useCallback } from "react";
 import { useMemo } from "react";
 import { useEffect, useState } from "react";
 import Card from "./components/Card";
+import ClearButton from "./components/ClearButton";
 import Header from "./components/Header";
 import HeaderMemo from "./components/HeaderMemo";
 
@@ -29,6 +31,16 @@ function App() {
     setSearch(text)
   }
 
+ /*  const handleClear = () => {
+    setText("")
+    setSearch("")
+  } */
+
+  const handleClear = useCallback(() => {
+    setText("")
+    setSearch("")
+  }, [])
+
   return (
     <div className="container mt-2">
       <div>
@@ -49,8 +61,18 @@ function App() {
       <div className="row">
         <Card data={filteredData} />
       </div>
+      <hr />
+      <ClearButton handleClear={handleClear}/>
+      <br /><br />
     </div>
   );
 }
 
 export default App;
+
+//stateler her değiştiğinde component render olacağı için alt componentler de yeniden oluşturulur. bunu önlemenin yolu react memo kullanmak. memo bize sadece ilgili componente gönderilen değer değiştiğinde sardığı componenti tekrar render ettirir. faydasız kaldığı yer object tipli verilerdir. onu engellemenin yolu da useMemo kullanmak 
+
+//useMemo
+// Shallow comparison’da eğer karşılaştırılan tipler nesne (Object) ise içerisindeki değerleri değil referans değerleri karşılaştırılır. Eğer karşılaştırılan nesneler memory’de aynı adresi gösteriyorsa true göstermiyorsa false olarak değer döndürür.
+
+// Input alanına bir değer girdiğimiz zaman App.js tekrar render edildiği için filteredData tekrar oluşur. filteredData tekrardan oluştuğu için Card componentine göndermiş olduğumuz data her seferinde farklı bir adrese sahip olur. Bu yüzdende React.memo Card componentine ilk seferde göndermiş olduğumuz data array’inin tutulduğu adres ile render edildikten sonra gelen data array’inin farklı adreste bulunduğunu gördüğü için CArd componentini tekrar render eder. Props olarak verdiğimiz array değişmediği halde Card componentinin render edilmesini engelleyebilmek için useMemo kullanabiliriz.
